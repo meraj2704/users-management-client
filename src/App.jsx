9148a02350d1cb61d6b7548ac4c0fbcea1f5dafd
merajHossain <mh1669101@gmail.com>
2023-11-12 19:46:22 +0600

    send req for add data

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ function App() {
     const email = form.email.value;
     const user = {name,email};
     console.log(user);
+    fetch('http://localhost:5000/users',{
+      method : 'POST',
+      headers : {
+      'content-type':'application/json' 
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {console.log('inside post response',data)})
   }
 
   return (
